3f8dac31b9bee2f3b30cd6bf3fb58b989ff8592a
Apricity <89236552+ApricityZ@users.noreply.github.com>
2024-01-20 15:07:15 +0800

    Lec 9

--- Lecture8ObjectOrientedProgramming/season.py
+++ Lecture8ObjectOrientedProgramming/season.py
@@ -1,9 +1,11 @@
 from datetime import date
+import re
+import sys
 import inflect
 
 def main():
     season = Season.get()
-    minutes = Season.get_old()
+    print(season.get_age_minute())
 
 
 class Season:
@@ -14,13 +16,15 @@ class Season:
     @classmethod
     def get(cls):
         birth = input("Date of Birth: ")
+        if not re.search(r"^\d{4}-\d?\d-\d?\d$", birth):
+            sys.exit("Invalid date.")
+
         return cls(birth)
 
 
-    def count_lear_year(self, birth):
-        birth_year, _, _  = birth.split("-")
-        year = int(birth_year)
-        now_year = date.year
+    def count_lear_year(self, birthyear):
+        year = birthyear
+        now_year = int(date.today().year)
         count_lear = 0
         while year <= now_year:
             if (year % 4 == 0 and year % 100 != 0) or (year % 400 ==0):
@@ -29,12 +33,30 @@ class Season:
         return count_lear
 
 
-    def __sub__(self, other):
-        today = date.today()
+    def covert2int(self):
+        birthday = self.birth.split("-")
+        birthday = tuple(map(int, birthday))
+        if birthday[1] < 0 or birthday[1] > 12:
+            sys.exit()
+        if birthday[2] < 0 or birthday[2] > 31:
+            sys.exit()
+        return birthday
 
 
+    def get_age_minute(self):
+        birthday = self.covert2int()
+        num_lear_year = self.count_lear_year(birthday[0])
+        birthday = date(birthday[0], birthday[1], birthday[2])
+        diff_age = date.today() - birthday
 
+        diff_day = num_lear_year + int(diff_age.days)
+        diff_min = diff_day * 24 * 60
+        inf = inflect.engine()
+        min2words = inf.number_to_words(diff_min)
+        min2words = min2words.replace(" and", "").capitalize()
+        return min2words + " minutes"
 
+# todo
 
 
 
